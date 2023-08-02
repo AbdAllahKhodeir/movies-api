92634ca9e03b7613932442a8fbc0e0a39b50256e
AbdAllah Khodeir <abdallahkhodeir@gmail.com>
2023-08-02 17:35:50 -0600

    Added MovieController, MovieRepository, MovieService

--- src/main/java/com/abdallahkhodeir/movies/MovieRepository.java
+++ src/main/java/com/abdallahkhodeir/movies/MovieRepository.java
@@ -2,7 +2,11 @@ package com.abdallahkhodeir.movies;
 
 import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
+@Repository
 public interface MovieRepository extends MongoRepository<Movie, ObjectId> {
-
+    Optional<Movie> findMovieByImdbId(String imdbId);
 }
